461cc1a406262495501b151b7576ac6c0534d68e
Jerry <15170551+jerryguo1020@user.noreply.gitee.com>
2025-06-19 11:40:35 +0800

    学习了一些内容

---
+++ vue-pinia/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="title">
+  this is a count text!
+</div>
+
+<div class="content">
+  <div class="count1">每次递增1的结果是: {{ count.count }}</div>
+  <div class="count2">每次递增两倍的结果是: {{ count.doubleCount }}</div>
+  <button @click="count.increment()">点击按钮</button>
+</div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const count = useCounterStore();
+</script>
+
+<style>
+
+</style>
